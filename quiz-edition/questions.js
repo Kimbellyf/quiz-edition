3706c8437dce7f07a86a1cd28581461ca7ba8e7d
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2024-06-08 11:17:02 -0300

    Update questions.js

--- quiz-edition/questions.js
+++ quiz-edition/questions.js
@@ -1,63 +1,34 @@
 const questionsAll = [
     {
         question: "Dentro de qual elemento HTML colocamos o JavaScript?",
+        number: 1,
+        page: 1,
         answers: [
             { text: "<javascript>", correct: false },
             { text: "<js>", correct: false },
             { text: "<script>", correct: true },
             { text: "<scripting>", correct: false }
-        ]
+        ],
+        explication: ''
     },
+
     {
-        question: "Onde é o lugar correto para inserir JavaScript?",
+        question: "Dentro de qual elemento HTML colocamos o JavaScript?",
+        number: 1,
+        page: 1,
+        typeQuestion: 'select',
         answers: [
-            { text: "Tanto no <head> quanto no <body> está correto", correct: true },
-            { text: "No <body>", correct: false },
-            { text: "No <head>", correct: false },
-            { text: "Em outro lugar", correct: false }
-        ]
+            [
+                { text: "<javascript>", correct: false },
+                { text: "<js>", correct: false },
+            ],
+            [
+                { text: "<script>", correct: true },
+                { text: "<scripting>", correct: false }
+            ]
+        ],
+        explication: ''
     },
-    {
-        question: 'Qual é a sintaxe correta para se referir a um script externo chamado "xxx.js"',
-        answers: [
-            { text: '<script src="xxx.js">', correct: true },
-            { text: '<script href="xxx.js">', correct: false },
-            { text: '<script name="xxx.js">', correct: false },
-            { text: "Nenhuma das alternativas", correct: false }
-        ]
-    },
-    {
-        question: 'O arquivo JavaScript externo deve conter a tag <script>',
-        answers: [
-            { text: "Verdadeiro", correct: false },
-            { text: "Falso", correct: true }
-        ]
-    },
-    {
-        question: 'Como escrever "Hello World" numa caixa de alerta?',
-        answers: [
-            { text: 'msg("Hello World");', correct: false },
-            { text: 'alert("Hello World");', correct: true },
-            { text: 'msgBox("Hello World");', correct: false },
-            { text: 'alertBox("Hello World");', correct: false }
-        ]
-    },
-    {
-        question: 'Como podemos criar uma função no JavaScript?',
-        answers: [
-            { text: 'function:myFunction()', correct: false },
-            { text: 'function myFunction()', correct: true },
-            { text: 'function = myFunction()', correct: false },
-            { text: 'Nenhum desses códigos criaria uma função', correct: false }
-        ]
-    },
-    {
-        question: 'Como podemos chamar uma função chamada "minhaFuncao"?',
-        answers: [
-            { text: 'call minhaFuncao()', correct: false },
-            { text: 'call function minhaFuncao()', correct: false },
-            { text: 'Nenhum desses códigos chamaria essa função', correct: false },
-            { text: 'minhaFuncao()', correct: true },
-        ]
-    },
+
+
 ]
